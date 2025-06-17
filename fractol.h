bedc27eae6529f0c2d10f8fc3b425b3c404e920c
Alois <amarti@student.42lehavre.fr>
2025-06-17 20:00:27 +0200

    debut

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: amarti <amarti@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 16:04:13 by amarti            #+#    #+#             */
-/*   Updated: 2025/06/16 20:41:46 by amarti           ###   ########.fr       */
+/*   Updated: 2025/06/17 19:59:28 by amarti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,37 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "libft.h"
-// #include "minilbx"
+#include <math.h>
+#include "MLX42/MLX42.h"
 #include <stdio.h>
+
+//constantes 
+#define WIDTH 800
+#define HEIGHT 800
+#define MAX_ITERATIONS 80
+
+//Types de fractales
+#define MANDELBROT 1
+#define JULIA 2
+
+typedef struct s_fractal
+{
+	int		type;
+	double	julia_x;
+	double	julia_y;
+	double	min_x;
+	double	max_x;
+	double	min_y;
+	double	max_y;
+	double	zoom;
+
+	mlx_t		*mlx;
+	mlx_image_t	*image;
+}	t_fractal;
+
+// Prototypes des fonctions
+double	atodbl(char *s);
+int		mandelbrot_set(double x, double y);
+int		julia_set(double x, double y, double julia_x, double julia_y);
+void	init_fractal(t_fractal *fractal, int type);
+double	map(double unscaled_num, double nw_min, double nw_max, double old_max);
--- utils.c
+++ utils.c
@@ -6,8 +6,80 @@
 /*   By: amarti <amarti@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 14:36:26 by amarti            #+#    #+#             */
-/*   Updated: 2025/06/16 16:08:06 by amarti           ###   ########.fr       */
+/*   Updated: 2025/06/17 19:59:28 by amarti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+int	handle_decimal_part(char *s, int i, double *result)
+{
+	double	diviseur;
+
+	diviseur = 10.0;
+	i++;
+	while(s[i] >= 48 && s[i] <= 57)
+	{
+		*result = *result + (s[i] - '0') / diviseur;
+		diviseur = diviseur * 10;
+		i++;
+	}
+	return (i);
+}
+double	atodbl(char *s)
+{
+	double	result;
+	int	nega;
+	int	i;
+
+	result = 0.0;
+	nega = 1;
+	i = 0;
+	while(s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
+		i++;
+	if(s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			nega = -1;
+		i++;
+	}
+	while(s[i] >= 48 && s[i] <= 57)
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	if(s[i] == '.')
+		i = handle_decimal_part(s, i, &result);
+	return (result * nega);
+}
+void	init_fractal(t_fractal *fractal, int type)
+{
+	fractal->type = type;
+	fractal->min_x = -2.0;
+	fractal->max_x = 2.0;
+	fractal->min_y = -2.0;
+	fractal->max_y = 2.0;
+	fractal->zoom = 1.0;
+	if (type == JULIA)
+	{
+		fractal->julia_x = 0.0;
+		fractal->julia_y = 0.0;
+	}
+	fractal->mlx = mlx_init(WIDTH, HEIGHT, "fractol", false);
+	if (fractal->mlx == NULL)
+	{
+		perror("Error: MLX initialization failed");
+		exit(1);
+	}
+	fractal->image = mlx_new_image(fractal->mlx, WIDTH, HEIGHT);
+	if(fractal->image == NULL)
+	{
+		perror("Error: image creation failed");
+		mlx_terminate(fractal->mlx);
+		exit(1);
+	}
+}
+double map(double unscaled_num, double nw_min, double nw_max, double old_max)
+{
+
+}
